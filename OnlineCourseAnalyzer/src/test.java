85feade62771772cb8d30b031adcf7fa6ae34bab
Florescence <93757463+Florescence@users.noreply.github.com>
2023-03-12 15:38:54 +0800

    Final version of this assignment
    LocalTest and OJ passed. Further perfection can be made in stream programming.

--- OnlineCourseAnalyzer/src/test.java
+++ OnlineCourseAnalyzer/src/test.java
@@ -1,8 +1,10 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class test {
 
-    static OnlineCourseAnalyzer onlineCourseAnalyzer = new OnlineCourseAnalyzer("./src/local.csv");
+    static OnlineCoursesAnalyzer onlineCoursesAnalyzer = new OnlineCoursesAnalyzer("./src/local.csv");
     static Scanner in = new Scanner(System.in);
 
     public static void test0() {
@@ -11,17 +13,28 @@ public class test {
 
     public static void test1() {
         System.out.println("1. Participants count by institution");
-        System.out.println(onlineCourseAnalyzer.getPtcpCountByInst());
+        System.out.println(onlineCoursesAnalyzer.getPtcpCountByInst());
     }
 
     public static void test2() {
         System.out.println("2. Participants count by institution and course subject");
-        System.out.println(onlineCourseAnalyzer.getPtcpCountByInstAndSubject());
+        System.out.println(onlineCoursesAnalyzer.getPtcpCountByInstAndSubject());
     }
 
     public static void test3() {
         System.out.println("3. Course list by Instructor");
-        System.out.println(onlineCourseAnalyzer.getCourseListOfInstructor());
+//        Set<String> instructorSet = onlineCoursesAnalyzer.getCourseListOfInstructor().keySet();
+//        Set<String> set = new HashSet();
+//        for (String instructor: instructorSet) {
+//            if (set.contains(instructor.trim())){
+//                System.out.println("contained");
+//            }
+//            else{
+//                set.add(instructor.trim());
+//            }
+//        }
+        System.out.println(onlineCoursesAnalyzer.getCourseListOfInstructor().size());
+        onlineCoursesAnalyzer.getCourseListOfInstructor().forEach((k,v) -> System.out.println(k + " == " + v));
     }
 
     public static void test4() throws NoSuchMethodException {
@@ -30,7 +43,7 @@ public class test {
         int topK = in.nextInt();
         System.out.println("Please enter by [a String (hours / participants)]:");
         String by = in.next();
-        System.out.println(onlineCourseAnalyzer.getCourses(topK, by));
+        System.out.println(onlineCoursesAnalyzer.getCourses(topK, by));
     }
 
     public static void test5() {
@@ -42,7 +55,7 @@ public class test {
         System.out.println("Please enter totalCourseHours (a double):");
         double totalCourseHours = in.nextDouble();
         System.out.println(
-            onlineCourseAnalyzer.searchCourses(courseSubject, percentAudited, totalCourseHours));
+            onlineCoursesAnalyzer.searchCourses(courseSubject, percentAudited, totalCourseHours));
     }
 
     public static void test6() {
@@ -54,16 +67,17 @@ public class test {
         System.out.println(
             "Please enter isBachelorOrHigher (0-Not get bachelor degree, 1- Bachelor degree or higher):");
         int isBachelorOrHigher = in.nextInt();
-        System.out.println(onlineCourseAnalyzer.recommendCourses(age, gender, isBachelorOrHigher));
+
+        System.out.println(onlineCoursesAnalyzer.recommendCourses(age, gender, isBachelorOrHigher));
     }
 
     public static void main(String[] args) throws NoSuchMethodException {
-        test0();
-        test1();
-        test2();
-        test3();
-        test4();
-        test5();
+        //test0();
+        //test1();
+        //test2();
+        //test3();
+        //test4();
+        //test5();
         test6();
     }
 }
